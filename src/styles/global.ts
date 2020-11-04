9ec7bde521593a8501b56e4a6f1702425df08fcf
Guilherme-Humberto <gbreu@outlook.com>
2020-11-04 14:12:29 -0300

    Estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        margin: 0,
+        padding: 0,
+        flex: 1,
+        backgroundColor: "#00171F"
+    }
+})
